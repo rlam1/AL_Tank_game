e224978a80f9d0878b3eab3d60695b37527903eb
Rodolfo Lam <r_lam@hotmail.com>
2014-02-28 22:33:15 -0500

    Start project rebuild without *.TMX support planned

--- Game/main.cpp
+++ Game/main.cpp
@@ -3,25 +3,30 @@
 #include <allegro5\allegro_font.h>
 #include <allegro5\allegro_ttf.h>
 #include <allegro5\allegro_native_dialog.h>
+#include <allegro5\allegro_primitives.h>
 #include <allegro5\keyboard.h>
 #include <string>
 
+#include "Game.h"
+
 #ifdef _DEBUG
-#include <iostream>
+#include "Debug.h"
 #endif
 
 int main(int argc, char* argv [])
 {
     al_init();
     al_init_image_addon();
+    al_init_primitives_addon();
     al_install_keyboard();
 
     ALLEGRO_DISPLAY *display = al_create_display(640, 480);
 
-    al_clear_to_color(al_map_rgb(0, 0, 0));
+    al_clear_to_color(al_map_rgb(255, 0, 0));
     al_flip_display();
 
     bool done = false;
+    bool redraw = false;
 
     ALLEGRO_TIMER *timer = al_create_timer(1.0 / 60.0);
 
@@ -31,6 +36,23 @@ int main(int argc, char* argv [])
     al_register_event_source(queue, al_get_display_event_source(display));
     al_register_event_source(queue, al_get_timer_event_source(timer));
 
+#ifdef _DEBUG
+    Debug_ deb;
+#endif
+
+    //**********************
+    ALLEGRO_BITMAP *test = al_load_bitmap("data/background.png");
+#ifdef _DEBUG
+    char ch_test_[33];
+    _itoa_s((int) test, ch_test_, 16);
+    deb.log_("Image HEX =", ch_test_);
+#endif
+
+    int x = 320, y = 240;
+    //*********************
+
+    al_start_timer(timer);
+
     while (!done)
     {
         ALLEGRO_EVENT ev;
@@ -38,6 +60,9 @@ int main(int argc, char* argv [])
 
         switch (ev.type)
         {
+            case ALLEGRO_EVENT_TIMER:
+                redraw = true;
+                break;
             case ALLEGRO_EVENT_DISPLAY_CLOSE:
                 done = true;
                 break;
@@ -46,21 +71,38 @@ int main(int argc, char* argv [])
                 {
                     done = true;
                 }
+                //************
                 else
                 {
+                    switch (ev.keyboard.keycode)
+                    {
+                        case ALLEGRO_KEY_LEFT:
+                            x--;
+                            break;
+                        case ALLEGRO_KEY_RIGHT:
+                            x++;
+                            break;
+                        default:
 #ifdef _DEBUG
-                    char key_id[33];
-                    _itoa_s(ev.keyboard.keycode, key_id, 10);
-                    std::cout << key_id << " pressed" << std::endl;
+                            deb.log_("Invalid input", deb.intToStr(ev.keyboard.keycode));
 #endif
+                    }
                 }
-                break;
-            case ALLEGRO_EVENT_TIMER:
-                al_clear_to_color(al_map_rgb(255,255,0));
-                al_flip_display();
+                //************
                 break;
             default:
                 break;
+        }
+
+        if (redraw && al_is_event_queue_empty(queue))
+        {
+            redraw = false;
+
+            al_clear_to_color(al_map_rgb(0, 0, 0));
+            al_draw_bitmap(test, 0, 0, NULL);
+
+            al_draw_filled_circle(x, 240, 89, al_map_rgb(255, 0, 255));
+            al_flip_display();
         }
     }
 
